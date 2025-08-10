42faf36648bb9fe6690f58d17b2f2db6344a1e8e
Apurba Barai <meapurba5@gmail.com>
2025-08-11 00:08:59 +0600

    A view named Overview added

--- FirstMVC/Controllers/ItemsController.cs
+++ FirstMVC/Controllers/ItemsController.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirstMVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FirstMVC.Controllers
 {
     public class ItemsController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Overview()
         {
-            return View();
+            var item=new Item() { Id = 1 ,Name="Laptop"};
+            return View(item);
         }
     }
 }
