b4ed5f7dba74d97d087103988d3dd874132986d3
Apurba Barai <meapurba5@gmail.com>
2025-08-11 15:38:17 +0600

    Database Migration with Item table added to DB

--- FirstMVC/Data/MyDbContext.cs
+++ FirstMVC/Data/MyDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FirstMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FirstMVC.Data
 {
     public class MyDbContext:DbContext
     {
         public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        public DbSet<Item> Items { get; set; }
     }
 }
